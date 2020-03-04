18518c0adce5775bf921029bdde4fb3eaaee5e05
Tai Klein <etai@hioscar.com>
2020-03-04 14:36:15 -0500

    init

--- extension.js
+++ extension.js
@@ -1,37 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-const fs = require("fs");
-const path = require("path");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function getEditor() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+  return editor;
+}
+
+function getSelectedText(editor) {
+  const selection = editor.selection;
+  let text = editor.document.getText(selection);
+  return text;
+}
+
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    '"react-intl-string-to-formatted-message" is now active! Highlight text to format'
+  const formatMessage = vscode.commands.registerCommand(
+    "extension.formattedMessage",
+    function() {
+      const editor = getEditor();
+      const code = getSelectedText(editor);
+      const fileName = ""; // TODO
+
+      editor.edit(edit => {
+        edit.replace(
+          editor.selection,
+          `<FormattedMessage defaultMessage="${code}" description="" id="${fileName}"/>`
+        );
+      });
+    }
   );
 
-  // 👍 formatter implemented using API
-  vscode.languages.registerDocumentFormattingEditProvider("foo-lang", {
-    provideDocumentFormattingEdits(document) {
-      const firstLine = document.lineAt(0);
-      if (firstLine.text !== "42") {
-        return [vscode.TextEdit.insert(firstLine.range.start, "42\n")];
-      }
-    }
-  });
-
-  // context.subscriptions.push(disposable);
+  context.subscriptions.push(formatMessage);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
