fd85b5c15f8aaa8063eec3940e13bac1b964e63a
Tai Klein <etai@hioscar.com>
2020-03-04 15:45:14 -0500

    autofill id

--- extension.js
+++ extension.js
@@ -15,18 +15,56 @@ function getSelectedText(editor) {
   return text;
 }
 
+function hashId(text = "") {
+  let hash = 0;
+  let i = 0;
+  let char = 0;
+  if (text.length == 0) return hash;
+  for (i = 0, l = text.length; i < l; i++) {
+    char = text.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+function generateId(editor, originalText) {
+  let id = vscode.workspace.asRelativePath(editor.document.fileName);
+
+  // remove filename extensions
+  id = id
+    .split(".")
+    .slice(0, -1)
+    .join("");
+
+  // change '/' to '.'
+  id = id.split("/").join(".");
+
+  // TODO move whitelist to settings config
+  // remove whitelisted paths
+  const whitelistedPaths = [
+    "mobile.member.js.",
+    "python.manhattan.static.js.",
+    "python.manhattan.oscar."
+  ];
+  whitelistedPaths.forEach(path => {
+    id = id.replace(path, "");
+  });
+  return id;
+}
+
 function activate(context) {
   const formatMessage = vscode.commands.registerCommand(
     "extension.formattedMessage",
     function() {
       const editor = getEditor();
-      const code = getSelectedText(editor);
-      const fileName = ""; // TODO
+      const originalText = getSelectedText(editor);
+      const id = `${generateId(editor, originalText)}.${hashId(originalText)}`;
 
       editor.edit(edit => {
         edit.replace(
           editor.selection,
-          `<FormattedMessage defaultMessage="${code}" description="" id="${fileName}"/>`
+          `<FormattedMessage defaultMessage="${originalText}" description="" id="${id}"/>`
         );
       });
     }
